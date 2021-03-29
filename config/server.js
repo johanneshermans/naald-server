2d7e1854f36055134e100b57baa7d57fed193be3
johanneshermans <johannes.hermans@student.howest.be>
2021-03-29 10:55:08 +0200

    all commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dea52886e36e975e60c67025cb9f0672'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '826781df-3272-4336-8eb7-a7041cd0acf1'
+};
